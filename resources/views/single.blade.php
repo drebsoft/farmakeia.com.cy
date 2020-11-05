5460265c62f9cbec999d753cfa0d4988fd84cd6a
gregkos <gregkos.original@gmail.com>
2020-11-05 17:01:28 +0200

    Single is actually pretty much ready

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,6 +1,6 @@
 <iframe
     width="650" height="400"
-    frameborder="0" style="border:0"
+    style="border:0"
     src="https://www.google.com/maps/embed/v1/place?key={{ $maps_api_key }}
-    &q=Space+Needle,Seattle+WA" allowfullscreen>
+        &q=Iremias+17,Lakatamia+Nicosia&region=cy" allowfullscreen>
 </iframe>
