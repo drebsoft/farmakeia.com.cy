a471f6055e9260848c274b531273331f27bc0257
roumpa <eggonidou@gmail.com>
2020-11-06 21:31:10 +0200

    index and create view cleanup

--- resources/views/pharmacies/index.blade.php
+++ resources/views/pharmacies/index.blade.php
@@ -1,23 +1,15 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-     @if($pharmacy !== null)
-        {{ $pharmacy->name }}
-        {{ $pharmacy->town }}
-        {{ $pharmacy->municipality }}
-        {{ $pharmacy->address }}
-        {{ $pharmacy->add_address }}
-        {{ $pharmacy->phone }}
-        {{ $pharmacy->am }}
-     @else
-        No pharmacies available at the moment
-     @endif
-</body>
-</html>
+ @if($pharmacies !== null)
+    @foreach($pharmacies as $pharmacy)
+        <label>Name: </label>{{ $pharmacy->name }}
+        <ul>
+            <li>Town: {{ $pharmacy->town }}</li>
+            <li>Municipality: {{ $pharmacy->municipality }}</li>
+            <li>Address: {{ $pharmacy->address }}</li>
+            <li>Additional Address: {{ $pharmacy->add_address }}</li>
+            <li>Phone number: {{ $pharmacy->phone }}</li>
+            <li>AM: {{ $pharmacy->am }}</li>
+        </ul>
+    @endforeach
+@else
+   No pharmacies available at the moment
+@endif
