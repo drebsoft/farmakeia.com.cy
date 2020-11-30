3d1ec6fe01319b502312235b2c1046d70ecf9056
gregkos <gregkos.original@gmail.com>
2020-11-30 23:02:01 +0200

    Fallbacks for additional slots

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,12 +15,12 @@
 
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
-        {{ $scriptloads }}
+        {{ $scriptloads ?? '' }}
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
-        {{ $scripts }}
+        {{ $scripts ?? '' }}
     </body>
 </html>
