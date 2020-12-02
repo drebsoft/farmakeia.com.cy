0c7c653544bede18c83b72dedd22464dde908be5
gregkos <gregkos.original@gmail.com>
2020-12-02 22:48:36 +0200

    Footer legal basics

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,4 +1,9 @@
 <footer class="bg-white">
+    <div class="mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <p class="text-center text-base leading-6 text-gray-400">
+            Όλες οι πληροφορίες που παρέχονται στον παρόν ιστότοπο προέρχονται από τις ανακοινώσεις του Υπουργείου για τα εφημερεύοντα φαρμακεία. Το farmakeia.com.cy καταβάλλει κάθε προσπάθεια για επικαιροποίηση, όμως δεν αναλαμβάνει ουδεμία ευθύνη για την ακρίβεια των πληροφοριών που παρέχονται.
+        </p>
+    </div>
     <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div class="flex justify-center space-x-6 md:order-2">
             <a href="#" class="text-gray-400 hover:text-gray-500">
@@ -27,7 +32,7 @@
         </div>
         <div class="mt-8 md:mt-0 md:order-1">
             <p class="text-center text-base leading-6 text-gray-400">
-                &copy; 2020 farmakeia.com.cy. All rights reserved.
+                &copy; 2020 farmakeia.com.cy. Με επιφύλαξη κάθε δικαιώματος.
             </p>
         </div>
     </div>
