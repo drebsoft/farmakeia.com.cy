ac68d9e16349f35e30d237d82a06b69be6fd99b3
gregkos <gregkos.original@gmail.com>
2020-12-04 22:51:19 +0200

    Fill coordinates as well during seeding (slow)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \Artisan::call('pharmacies:update-availability');
+        \Artisan::call('pharmacies:fill-coordinates');
     }
 }
