0bbb5adf71d9cc331d98c575c7c35617dc1490bf
Chrysanthos Prodromou <chrysanthos.prodromou@gmail.com>
2020-12-06 22:50:09 +0200

    Add some tailwind love to privacy policy and how it works

--- resources/views/pages/about/politiki-aporritou.blade.php
+++ resources/views/pages/about/politiki-aporritou.blade.php
@@ -1,9 +1,59 @@
-@extends('pages.about.template')
+<x-guest-layout>
+    <div class="relative py-16 overflow-hidden">
+        <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+            <div class="relative h-full text-lg max-w-prose mx-auto">
+                <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none"
+                     viewBox="0 0 404 384">
+                    <defs>
+                        <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20"
+                                 patternUnits="userSpaceOnUse">
+                            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor"/>
+                        </pattern>
+                    </defs>
+                    <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"/>
+                </svg>
+                <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404"
+                     height="384" fill="none" viewBox="0 0 404 384">
+                    <defs>
+                        <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20"
+                                 patternUnits="userSpaceOnUse">
+                            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor"/>
+                        </pattern>
+                    </defs>
+                    <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"/>
+                </svg>
+                <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none"
+                     viewBox="0 0 404 384">
+                    <defs>
+                        <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20"
+                                 patternUnits="userSpaceOnUse">
+                            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor"/>
+                        </pattern>
+                    </defs>
+                    <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"/>
+                </svg>
+            </div>
+        </div>
+        <div class="relative px-4 sm:px-6 lg:px-8">
+            <div class="text-lg max-w-prose mx-auto mb-6">
+                <h1 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+                    Πολιτική Απορρήτου
+                </h1>
+            </div>
+            <div class="prose prose-lg text-gray-500 mx-auto">
+                <p>
+                    Το farmakeia.com.cy δεν συλλέγει, διατήρει, ή επεξεργάζεται προσωπικά δεδομένα των χρηστών του.
+                </p>
 
-@section('title', 'Πολιτική Απορρήτου')
+                <p>
+                Οι διακομιστές του farmakeia.com.cy ενδέχεται να αποθηκεύσουν προσωρινά στοιχεία πρόσβασης όπως η IP σας για σκοπούς προστασίας ενάντια σε ενδεχόμενες κακόβουλες επιθέσεις, όμως όλα τα στοιχεία παραμένουν ανώνυμα και διαγράφονται σε τακτά χρονικά διαστήματα.
+                </p>
 
-@section('content')
-    <p class="mb-3">Το farmakeia.com.cy δεν συλλέγει, διατήρει, ή επεξεργάζεται προσωπικά δεδομένα των χρηστών του.</p>
-    <p class="mb-3">Οι διακομιστές του farmakeia.com.cy ενδέχεται να αποθηκεύσουν προσωρινά στοιχεία πρόσβασης όπως η IP σας για σκοπούς προστασίας ενάντια σε ενδεχόμενες κακόβουλες επιθέσεις, όμως όλα τα στοιχεία παραμένουν ανώνυμα και διαγράφονται σε τακτά χρονικά διαστήματα.</p>
-    <p class="mb-3">Το farmakeia.com.cy δεν χρησιμοποιεί cookies για οποιαδήποτε από τις λειτουργίες του, όμως συνεργάζεται με τα Google Analytics για να αναλύσει συλλογικά τις προτιμήσεις των χρηστών του και να εξελιχθεί. Σε καμία περίπτωση δεν γίνεται μεμονωμένη κατάγραφή, αλλά όλα τα στοιχεία αναλύονται αθροιστικά και ανώνυμα.</p>
-@endsection
+                <p>
+                Το farmakeia.com.cy δεν χρησιμοποιεί cookies για οποιαδήποτε από τις λειτουργίες του, όμως συνεργάζεται με τα Google Analytics για να αναλύσει συλλογικά τις προτιμήσεις των χρηστών του και να εξελιχθεί. Σε καμία περίπτωση δεν γίνεται μεμονωμένη κατάγραφή, αλλά όλα τα στοιχεία αναλύονται αθροιστικά και ανώνυμα.
+                </p>
+            </div>
+        </div>
+    </div>
+
+</x-guest-layout>
