309a45ad1ba306e09392620d5ce63f1f01157963
Chrysanthos Prodromou <chrysanthos.prodromou@gmail.com>
2020-12-21 23:23:55 +0200

    Fix navigation z-index on privacy policy

--- resources/views/pages/about/politiki-aporritou.blade.php
+++ resources/views/pages/about/politiki-aporritou.blade.php
@@ -5,8 +5,8 @@
               content="Η πολιτική απορρήτου του farmakeia.com.cy.">
     </x-slot>
 
-    <div class="relative py-16 overflow-hidden">
-        <div class="relative px-4 sm:px-6 lg:px-8">
+    <div class=" py-16 overflow-hidden">
+        <div class=" px-4 sm:px-6 lg:px-8">
             <div class="text-lg max-w-prose mx-auto mb-6">
                 <h1 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
                     Πολιτική Απορρήτου
